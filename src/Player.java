f2ba741d9fd98663f1e945431c5c9a3ae7b7ee2f
KJ <zeal24460221@fsgcl.jp>
2025-06-17 10:36:44 +0900

    クラスの作成

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+
+public class Player extends Character{
+	
+	public Player(double x, double y, double vx, double vy) {
+		
+		super(x, y, vx, vy);
+		
+	}
+
+}
---
+++ src/PlayerBullet.java
@@ -0,0 +1,10 @@
+
+public class PlayerBullet extends Character{
+
+	public PlayerBullet(double x, double y, double vx, double vy) {
+		
+		super(x, y, vx, vy);
+		
+	}
+
+}
